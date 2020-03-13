2b0eaaa971d91c491a0138b087730339cb4054d9
facuferrara <facuferrara@hotmail.com>
2020-03-13 07:17:34 -0300

    falta sincronizar los strings tipo conca

--- index.js
+++ index.js
@@ -1,22 +1,87 @@
 const superagent = require('superagent');
+const fs = require('fs')
 
-function imprimirMuseos(error, respuesta) {
+'----------------------------------Debajo<!!!!> ---------------------------'
+
+function pegarleAApi(next){
+
+  superagent
+
+  .get('https://www.cultura.gob.ar/api/v2.0/museos')
+  .query({ format: 'json' })
+  .end(next)
+  
+}
+
+function guardarOEscribirArchivo(error, respuesta){
+
+  let listita=[]
+
   if (error) {
+    
     throw new Error('algo se rompió', error);
   }
 
-  const cantidad = respuesta.body.count;
-  const museos = respuesta.body.results;
+  console.log(respuesta.body.results[0].direccion ) 
 
-  console.log(`Se encontraron ${cantidad} museos.`);
-  console.log(`El primer museo se llama ${museos[0].nombre}.`)
+  const organismo = respuesta.body.results
+
+  for(let i = 0; organismo.length > i; i++){
+    
+    fs.appendFile('organismos.txt', organismo[i].direccion + '\n' ,terminar);
 }
 
-console.log('Antes de llamar a superagent')
 
-superagent
-  .get('https://www.cultura.gob.ar/api/v2.0/museos')
+
+    //console.log(respuesta.body.results[0].direccion )   armar todo y despues guardar todo
+    //fs.writeFile('museos.txt', respuesta.body.count ,terminar);
+    //fs.writeFile('museos.txt', respuesta.body.ne ,terminar);
+}
+
+function terminar(error){
+
+  console.log("listo todo a terminado");
+
+}
+'------------------------------------------------------------------------------------------------'
+                                        ///ORGANISMOS
+'------------------------------------------------------------------------------------------------'
+function pegarleAApi2(next){
+
+  superagent
+
+  .get('https://www.cultura.gob.ar/api/v2.0/organismos')
   .query({ format: 'json' })
-  .end(imprimirMuseos)
+  .end(next)
+  
+}
 
-console.log('Después de llamar a superagent')
+function guardarOEscribirArchivo2(error, respuesta){
+
+  if (error) {
+    
+    throw new Error('algo se rompió', error);
+  }
+    
+    fs.writeFile('organismos.txt', respuesta.body.results[1].id ,terminar);
+}
+
+function terminar2(error){
+
+  console.log("listo todo a terminado");
+
+}
+
+
+pegarleAApi(guardarOEscribirArchivo)
+//pegarleAApi2(guardarOEscribirArchivo2)
+
+//anotaciones:
+
+/*function DespuesDeEscribir(error) {
+  if (error) {
+    throw new Error('no se pudo escribir');
+  console.log('Todo joya ,  anda a leer tu archivo')
+  }
+
+fs.write('museos.txt', 'hola que tal ', DespuesDeEscribir); } */
